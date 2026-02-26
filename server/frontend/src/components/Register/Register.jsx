b8ae9d8e870e2c90b3315986d636fd9a16565540
Siphiwo-lembethe <autumnfalls604@gmail.com>
2026-02-26 17:07:44 +0200

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import "./Register.css";
-import user_icon from "../assets/person.png"
-import email_icon from "../assets/email.png"
-import password_icon from "../assets/password.png"
-import close_icon from "../assets/close.png"
 
 const Register = () => {
   const [userName, setUserName] = useState("");
@@ -21,7 +17,9 @@ const Register = () => {
     let register_url = window.location.origin + "/djangoapp/register";
     const res = await fetch(register_url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
         "userName": userName,
         "password": password,
@@ -41,42 +39,31 @@ const Register = () => {
   };
 
   return (
-    <div className="register_container" style={{width: "50%"}}>
-      <div className="header" style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-        <span className="text" style={{flexGrow: "1"}}>SignUp</span>
-        <div style={{display: "flex", flexDirection: "row", justifySelf: "end", alignSelf: "start"}}>
-          <a href="/" onClick={() => {gohome()}}><img style={{width: "1cm"}} src={close_icon} alt="X"/></a>
-        </div>
+    <div style={{display: "flex", justifyContent: "center", marginTop: "50px"}}>
+      <div style={{width: "400px", padding: "20px", border: "1px solid #ccc", borderRadius: "8px"}}>
+        <h2>Sign Up</h2>
+        <form onSubmit={register}>
+          <div style={{marginBottom: "10px"}}>
+            <input type="text" placeholder="Username" style={{width: "100%", padding: "8px"}} onChange={(e) => setUserName(e.target.value)} required />
+          </div>
+          <div style={{marginBottom: "10px"}}>
+            <input type="text" placeholder="First Name" style={{width: "100%", padding: "8px"}} onChange={(e) => setFirstName(e.target.value)} required />
+          </div>
+          <div style={{marginBottom: "10px"}}>
+            <input type="text" placeholder="Last Name" style={{width: "100%", padding: "8px"}} onChange={(e) => setlastName(e.target.value)} required />
+          </div>
+          <div style={{marginBottom: "10px"}}>
+            <input type="email" placeholder="Email" style={{width: "100%", padding: "8px"}} onChange={(e) => setEmail(e.target.value)} required />
+          </div>
+          <div style={{marginBottom: "10px"}}>
+            <input type="password" placeholder="Password" style={{width: "100%", padding: "8px"}} onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <button type="submit" style={{width: "100%", padding: "10px", backgroundColor: "darkturquoise", color: "white", border: "none", borderRadius: "4px", cursor: "pointer"}}>Register</button>
+        </form>
+        <p style={{marginTop: "10px"}}><a href="/" onClick={gohome}>Back to Home</a></p>
       </div>
-      <form onSubmit={register}>
-        <div className="inputs">
-          <div className="input">
-            <img src={user_icon} className="img_icon" alt='Username'/>
-            <input type="text" name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)}/>
-          </div>
-          <div>
-            <img src={user_icon} className="img_icon" alt='First Name'/>
-            <input type="text" name="first_name" placeholder="First Name" className="input_field" onChange={(e) => setFirstName(e.target.value)}/>
-          </div>
-          <div>
-            <img src={user_icon} className="img_icon" alt='Last Name'/>
-            <input type="text" name="last_name" placeholder="Last Name" className="input_field" onChange={(e) => setlastName(e.target.value)}/>
-          </div>
-          <div>
-            <img src={email_icon} className="img_icon" alt='Email'/>
-            <input type="email" name="email" placeholder="email" className="input_field" onChange={(e) => setEmail(e.target.value)}/>
-          </div>
-          <div className="input">
-            <img src={password_icon} className="img_icon" alt='password'/>
-            <input name="psw" type="password" placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)}/>
-          </div>
-        </div>
-        <div className="submit_panel">
-          <input className="submit" type="submit" value="Register"/>
-        </div>
-      </form>
     </div>
-  )
+  );
 }
 
 export default Register;
