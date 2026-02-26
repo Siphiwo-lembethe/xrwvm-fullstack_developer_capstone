71855ada1577aa576201beccd8ed1d01e0116bf9
Siphiwo-lembethe <autumnfalls604@gmail.com>
2026-02-26 17:08:14 +0200

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,10 @@
-import LoginPanel from "./components/Login/Login"
-import Register from "./components/Register/Register"
-import { Routes, Route } from "react-router-dom";
-function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  );
-}
-export default App;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './components/Register/Register';
+
+// inside return():
+<BrowserRouter>
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+  </Routes>
+</BrowserRouter>
