b22b848d197e4976af3985332f25d598a9c1c831
Siphiwo-lembethe <autumnfalls604@gmail.com>
2026-03-03 11:06:56 -0500

    Add CI/CD linting workflow and dealer routes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPanel from './components/Login/Login';
 import Register from './components/Register/Register';
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from './components/Dealers/PostReview';
 
 const Home = () => {
   return <div>Home - redirecting...</div>;
@@ -14,9 +17,11 @@ const App = () => {
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
+        <Route path="/dealers" element={<Dealers />} />
+        <Route path="/dealer/:id" element={<Dealer />} />
+        <Route path="/postreview/:id" element={<PostReview />} />
       </Routes>
     </Router>
   );
 };
-
 export default App;
